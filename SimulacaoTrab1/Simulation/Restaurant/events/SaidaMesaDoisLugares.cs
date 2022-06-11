2bdbccbf5f5d04283d668d2d7575b9e45b79d0d3
guil-pohlmann <56002372+guil-pohlmann@users.noreply.github.com>
2022-06-11 20:08:42 -0300

    Create SaidaMesaDoisLugares.cs

---
+++ SimulacaoTrab1/Simulation/Restaurant/events/SaidaMesaDoisLugares.cs
@@ -0,0 +1,34 @@
+﻿using SimulacaoTrab1.Simulation.Model;
+using SimulacaoTrab1.Simulation.Restaurant.entities;
+
+namespace SimulacaoTrab1.Simulation.Restaurant.events
+{
+    public class SaidaMesaDoisLugares : Event
+    {
+        public EntitySet QueueTableForTwo { get; set; }
+        public Resource TableForTwo { get; set; }
+        public Entity ClientGroup { get; set; }
+
+        public SaidaMesaDoisLugares(string name, Entity clientGroup, Resource resource, Scheduler scheduler)
+        : base(name)
+        {
+            this.Resource = resource;
+            this.Scheduler = scheduler;
+
+            this.QueueTableForTwo = Scheduler.GetEntitySetByName("filaMesa2Lugares");
+            this.TableForTwo = Scheduler.GetResourceByName("mesa2Lugares");
+            this.ClientGroup = clientGroup;
+        }
+
+        public new void Execute()
+        {
+            base.Execute();
+            TableForTwo.Release(1);
+            Scheduler.DestroyEntity(ClientGroup.Id);
+            if (QueueTableForTwo.Entities.Count > 0)
+            {
+                Scheduler.ScheduleNow(Scheduler.CreateEvent(new ChegadaMesaDoisLugares("Chegada 2 lugares", (ClientGroup)QueueTableForTwo.Remove(), Scheduler)));
+            }
+        }
+    }
+}
