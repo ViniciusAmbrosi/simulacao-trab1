2ea716d3915bf661331eecf99247af7f6066f207
guil-pohlmann <56002372+guil-pohlmann@users.noreply.github.com>
2022-06-11 20:08:15 -0300

    Create ClientGroup.cs

---
+++ SimulacaoTrab1/Simulation/Restaurant/entities/ClientGroup.cs
@@ -0,0 +1,15 @@
+﻿using SimulacaoTrab1.Simulation.Model;
+
+namespace SimulacaoTrab1.Simulation.Restaurant.entities
+{
+    public class ClientGroup : Entity
+    {
+        public int Quantity { get; set; }
+
+        public ClientGroup(string name, int quantity, Scheduler scheduler)
+        : base(name, scheduler)
+        {
+            this.Quantity = quantity;
+        }
+    }
+}
