aa4a0ad5a5c63f54162ba1fd6ae8d6d0786b75fc
guil-pohlmann <56002372+guil-pohlmann@users.noreply.github.com>
2022-06-11 20:08:37 -0300

    Create SaidaBalcao.cs

---
+++ SimulacaoTrab1/Simulation/Restaurant/events/SaidaBalcao.cs
@@ -0,0 +1,31 @@
+﻿using SimulacaoTrab1.Simulation.Model;
+using SimulacaoTrab1.Simulation.Restaurant.entities;
+
+namespace SimulacaoTrab1.Simulation.Restaurant.events
+{
+    public class SaidaBalcao : Event
+    {
+        public EntitySet QueueDesk { get; set; }
+        public Resource Desk { get; set; }
+        public Entity ClientGroup { get; set; }
+
+        public SaidaBalcao(string name, Entity clientGroup, Resource resource, Scheduler scheduler)
+        : base(name, resource, scheduler)
+        {
+            this.QueueDesk = Scheduler.GetEntitySetByName("filaBalcao");
+            this.Desk = Scheduler.GetResourceByName("balcao");
+            this.ClientGroup = clientGroup;
+        }
+
+        public new void Execute()
+        {
+            base.Execute();
+            Desk.Release(1);
+            Scheduler.DestroyEntity(ClientGroup.Id);
+            if (QueueDesk.Entities.Count > 0)
+            {
+                Scheduler.ScheduleNow(Scheduler.CreateEvent(new ChegadaBalcao("Chegada 1 lugar", (ClientGroup)QueueDesk.Remove(), Scheduler)));
+            }
+        }
+    }
+}
