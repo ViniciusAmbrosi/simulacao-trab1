60ace1e0ff6fe5cfa7ad22386f9f0808324e98bc
guil-pohlmann <56002372+guil-pohlmann@users.noreply.github.com>
2022-06-11 20:08:17 -0300

    Create AtendimentoCaixa.cs

---
+++ SimulacaoTrab1/Simulation/Restaurant/events/AtendimentoCaixa.cs
@@ -0,0 +1,27 @@
+﻿using SimulacaoTrab1.Simulation.Model;
+
+namespace SimulacaoTrab1.Simulation.Restaurant.events
+{
+    public class AtendimentoCaixa : Event
+    {
+        public AtendimentoCaixa(string name, Resource cashier, EntitySet cashierQueue, Scheduler scheduler)
+        : base(name, cashier, scheduler)
+        {
+        }
+
+        public new void Execute()
+        {
+            base.Execute();
+            if (EntitySet.Entities.Count > 0)
+            {
+
+                if (Resource.Allocate(1))
+                {
+                    //conseguiu alocar caixa pra atender
+                    //Agenda final do atendimento em normal (8,2) minutos
+                    Scheduler.ScheduleIn(Scheduler.CreateEvent(new FinalizarAtendimentoCaixa("Finalizar atendimento caixa", Resource, EntitySet, Scheduler)), Scheduler.Normal(8, 2));
+                }
+            }
+        }
+    }
+}
