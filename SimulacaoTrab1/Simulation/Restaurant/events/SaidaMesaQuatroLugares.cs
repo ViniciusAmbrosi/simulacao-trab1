929050d7a775115a27fc9c0962813eaca408861d
guil-pohlmann <56002372+guil-pohlmann@users.noreply.github.com>
2022-06-11 20:08:45 -0300

    Create SaidaMesaQuatroLugares.cs

---
+++ SimulacaoTrab1/Simulation/Restaurant/events/SaidaMesaQuatroLugares.cs
@@ -0,0 +1,31 @@
+﻿using SimulacaoTrab1.Simulation.Model;
+using SimulacaoTrab1.Simulation.Restaurant.entities;
+
+namespace SimulacaoTrab1.Simulation.Restaurant.events
+{
+    public class SaidaMesaQuatroLugares : Event
+    {
+        public EntitySet QueueTableForFour { get; set; }
+        public Resource TableForFour { get; set; }
+        public Entity ClientGroup { get; set; }
+
+        public SaidaMesaQuatroLugares(string name, Entity clientGroup, Resource resource, Scheduler scheduler)
+        : base(name, resource, scheduler)
+        {
+            this.QueueTableForFour = Scheduler.GetEntitySetByName("filaMesa4Lugares");
+            this.TableForFour = Scheduler.GetResourceByName("mesa4Lugares");
+            this.ClientGroup = clientGroup;
+        }
+
+        public new void Execute()
+        {
+            base.Execute();
+            TableForFour.Release(1);
+            Scheduler.DestroyEntity(ClientGroup.Id);
+            if (QueueTableForFour.Entities.Count > 0)
+            {
+                Scheduler.ScheduleNow(Scheduler.CreateEvent(new ChegadaMesaQuatroLugares("Chegada 4 lugares", (ClientGroup)QueueTableForFour.Remove(), Scheduler)));
+            }
+        }
+    }
+}
